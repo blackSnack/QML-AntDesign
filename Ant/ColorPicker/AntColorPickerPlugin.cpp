2599baa93f01eca9a5c0062b616a2e8c4cc6799b
yunfeng <youngfeng.gao@icloud.com>
2024-02-07 09:22:05 +0800

    添加AntColorPicker plugin

---
+++ Ant/ColorPicker/AntColorPickerPlugin.cpp
@@ -0,0 +1,7 @@
+﻿#include "AntColorPickerPlugin.hpp"
+#include <qqml.h>
+
+void AntColorPickerPlugin::registerTypes(const char *uri)
+{
+    qmlRegisterType(QUrl("qrc:/AntColorPicker/AntColorPicker.qml"), uri ,1, 0, "AntColorPicker");
+}
