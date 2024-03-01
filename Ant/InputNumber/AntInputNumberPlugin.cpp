954d11ad59805f6b4594c04a195e9594489106ff
yunfeng <youngfeng.gao@icloud.com>
2024-03-01 11:13:02 +0800

    添加AntInputNumber 组件工程

---
+++ Ant/InputNumber/AntInputNumberPlugin.cpp
@@ -0,0 +1,7 @@
+﻿#include "AntInputNumberPlugin.hpp"
+#include <qqml.h>
+
+void AntInputNumberPlugin::registerTypes(const char *uri)
+{
+    qmlRegisterType(QUrl("qrc:/AntInputNumber/AntInputNumber.qml"), uri ,1, 0, "AntInputNumber");
+}
