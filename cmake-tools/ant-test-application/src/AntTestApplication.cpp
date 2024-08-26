a06ee8f5c2c1260e7248fd13500aa2bc1d47ee49
yunfeng <youngfeng.gao@icloud.com>
2024-08-26 16:10:33 +0800

    Add AntTestApplication to start ant unittest application
    * if Qt version is less 6 will enable high dpi configuration.

---
+++ cmake-tools/ant-test-application/src/AntTestApplication.cpp
@@ -0,0 +1,37 @@
+﻿#include "ant/AntTestApplication.hpp"
+#include <QDebug>
+
+namespace Ant {
+AntTestApplication::AntTestApplication(int& argc, char **argv)
+    : enabledHighDpiM{ []() {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+          QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+          qDebug() << "Enabled high DPI scaling";
+#endif
+          return true;
+      }() },
+      guiAppM{ argc, argv }
+{
+    engineM.addImportPath("plugins");
+    engineM.addImportPath("../plugins");
+}
+
+AntTestApplication::~AntTestApplication() {}
+
+void AntTestApplication::load(const QUrl& url)
+{
+    QObject::connect(
+        &engineM,
+        &QQmlApplicationEngine::exit,
+        &guiAppM,
+        [](int code) { QCoreApplication::exit(code); },
+        Qt::QueuedConnection);
+    engineM.load(url);
+}
+
+int AntTestApplication::exec()
+{
+    return guiAppM.exec();
+}
+
+} // namespace Ant
