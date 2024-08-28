4730b9ab49ef8586360ece079e332cd9d5487e09
yunfeng <youngfeng.gao@icloud.com>
2024-08-28 16:06:26 +0800

    Add empty AntSelect plugin

---
+++ Ant/Select/AntSelectPlugin.cpp
@@ -0,0 +1,8 @@
+﻿#include "AntSelectPlugin.hpp"
+#include <qqml.h>
+
+void AntSelectPlugin::registerTypes(const char *uri)
+{
+    qmlRegisterType(QUrl("qrc:/AntSelect/AntSelect.qml"), uri ,1, 0, "AntSelect");
+    qmlRegisterType(QUrl("qrc:/Style/AntSelect/AntSelectStyle.qml"), uri ,1, 0, "AntSelectStyle");
+}
