f223c8aea4cc81bf4d3b853b792d924c9dcf2fc6
yunfeng <youngfeng.gao@icloud.com>
2024-08-29 09:22:38 +0800

    Add empty AntTag component

---
+++ Ant/Tag/AntTagPlugin.cpp
@@ -0,0 +1,8 @@
+﻿#include "AntTagPlugin.hpp"
+#include <qqml.h>
+
+void AntTagPlugin::registerTypes(const char *uri)
+{
+    qmlRegisterType(QUrl("qrc:/AntTag/AntTag.qml"), uri ,1, 0, "AntTag");
+    qmlRegisterType(QUrl("qrc:/Style/AntTag/AntTagStyle.qml"), uri ,1, 0, "AntTagStyle");
+}
