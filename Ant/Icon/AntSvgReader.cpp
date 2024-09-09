d019c6df6dad0f6dbd73a979459fcce27b58be63
yunfeng <youngfeng.gao@icloud.com>
2024-09-09 15:27:32 +0800

    Modify AntSvgReader to support opacity color

--- Ant/Icon/AntSvgReader.cpp
+++ Ant/Icon/AntSvgReader.cpp
@@ -2,9 +2,9 @@
 
 #include <QFile>
 
-AntSvgReader::AntSvgReader(): primaryColorM {QColor("#333")}, secondaryColorM{QColor{"#D9D9D9"}} {}
+AntSvgReader::AntSvgReader() : primaryColorM{ QColor("#333") }, secondaryColorM{ QColor{ "#D9D9D9" } } {}
 
-void AntSvgReader::setPrimaryColor(const QColor &color)
+void AntSvgReader::setPrimaryColor(const QColor& color)
 {
     if (primaryColorM != color)
     {
@@ -18,7 +18,7 @@ QColor AntSvgReader::primaryColor() const
     return primaryColorM;
 }
 
-void AntSvgReader::setSecondaryColor(const QColor &color)
+void AntSvgReader::setSecondaryColor(const QColor& color)
 {
     if (secondaryColorM != color)
     {
@@ -32,7 +32,7 @@ QColor AntSvgReader::secondaryColor() const
     return secondaryColorM;
 }
 
-bool AntSvgReader::load(const QString &filePath)
+bool AntSvgReader::load(const QString& filePath)
 {
     QFile file(filePath);
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
@@ -52,10 +52,15 @@ bool AntSvgReader::loadData(const QString& rawData)
     return true;
 }
 
-QByteArray AntSvgReader::data() const {
+QByteArray AntSvgReader::data() const
+{
     QString tempData(dataM);
     tempData.replace("primaryColor", primaryColorM.name());
-    tempData.replace("secondaryColor", secondaryColorM.isValid() ? secondaryColorM.name() : originSecondaryColorM.name());
+    tempData.replace("primaryFillOpacity", QString::number(primaryColorM.alphaF(), 10, 2));
+    auto tmpSecondaryColor = secondaryColorM.isValid() ? secondaryColorM : originSecondaryColorM;
+    tempData.replace("secondaryColor", tmpSecondaryColor.name());
+    tempData.replace("secondaryFillOpacity", QString::number(tmpSecondaryColor.alphaF(), 10, 2));
+
     return tempData.toLocal8Bit();
 }
 
@@ -63,18 +68,21 @@ void AntSvgReader::parseData()
 {
     const QString primaryColorStr("\"primaryColor\"");
     dataM.replace("\"#333\"", primaryColorStr);
-    dataM.replace("path d", "path fill=\"primaryColor\" d");
-    if (dataM.contains("\"#E6E6E6\"", Qt::CaseInsensitive)) {
-        dataM.replace("\"#E6E6E6\"", "\"secondaryColor\"", Qt::CaseInsensitive);
+    dataM.replace("path d", "path fill=\"primaryColor\" fill-opacity=\"primaryFillOpacity\" d");
+    if (dataM.contains("\"#E6E6E6\"", Qt::CaseInsensitive))
+    {
+        dataM.replace("\"#E6E6E6\"", "\"secondaryColor\" fill-opacity=\"secondaryFillOpacity\"", Qt::CaseInsensitive);
         originSecondaryColorM = QColor("#E6E6E6");
     }
-    if (dataM.contains("\"#D9D9D9\"", Qt::CaseInsensitive)) {
-        dataM.replace("\"#D9D9D9\"", "\"secondaryColor\"", Qt::CaseInsensitive);
+    if (dataM.contains("\"#D9D9D9\"", Qt::CaseInsensitive))
+    {
+        dataM.replace("\"#D9D9D9\"", "\"secondaryColor\" fill-opacity=\"secondaryFillOpacity\"", Qt::CaseInsensitive);
         originSecondaryColorM = QColor("#D9D9D9");
     }
 
-    if (dataM.contains("\"#D8D8D8\"", Qt::CaseInsensitive)) {
-        dataM.replace("\"#D8D8D8\"", "\"secondaryColor\"", Qt::CaseInsensitive);
+    if (dataM.contains("\"#D8D8D8\"", Qt::CaseInsensitive))
+    {
+        dataM.replace("\"#D8D8D8\"", "\"secondaryColor\" fill-opacity=\"secondaryFillOpacity\"", Qt::CaseInsensitive);
         originSecondaryColorM = QColor("#D8D8D8");
     }
 }
