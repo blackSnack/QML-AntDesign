add2fafcfab19a9142187435f55e7eaed79ef364
yunfeng <youngfeng.gao@icloud.com>
2025-06-05 07:58:44 +0800

    Add AntItem

---
+++ Ant/Item/AntItem.qml
@@ -0,0 +1,7 @@
+import QtQuick 2.15
+
+MouseArea {
+    id: root
+
+    readonly property bool hovered: containsMouse
+}
---
+++ Ant/Item/AntItemPlugin.cpp
@@ -0,0 +1,7 @@
+﻿#include "AntItemPlugin.hpp"
+#include <qqml.h>
+
+void AntItemPlugin::registerTypes(const char *uri)
+{
+    qmlRegisterType(QUrl("qrc:/AntItem/AntItem.qml"), uri ,1, 0, "AntItem");
+}
