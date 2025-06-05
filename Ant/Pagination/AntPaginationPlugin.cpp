136ee5a1970d6618a1e0d79e592e80f1dc958fdc
yunfeng <youngfeng.gao@icloud.com>
2025-06-05 10:50:06 +0800

    Init Pagination plugin

---
+++ Ant/Pagination/AntPaginationPlugin.cpp
@@ -0,0 +1,8 @@
+﻿#include "AntPaginationPlugin.hpp"
+#include <qqml.h>
+
+void AntPaginationPlugin::registerTypes(const char *uri)
+{
+    qmlRegisterType(QUrl("qrc:/AntPagination/AntPagination.qml"), uri ,1, 0, "AntPagination");
+    qmlRegisterType(QUrl("qrc:/Style/AntPagination/AntPaginationStyle.qml"), uri ,1, 0, "AntPaginationStyle");
+}
